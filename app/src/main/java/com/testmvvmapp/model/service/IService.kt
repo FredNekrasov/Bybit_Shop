9e7fc7d50942237df8cb01121558280e9c3e57c7
FredNekrasov <frednekrasov49@gmail.com>
2024-03-01 20:42:50 +0300

    refactoring an IService

--- app/src/main/java/com/testmvvmapp/model/service/IService.kt
+++ app/src/main/java/com/testmvvmapp/model/service/IService.kt
@@ -1,12 +1,11 @@
 package com.testmvvmapp.model.service
 
 import com.testmvvmapp.model.service.dto.ByBitDTO
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface IService {
     @GET("/v5/announcements/index?locale=en-US&limit=10")
-    fun get(): Call<ByBitDTO>
+    suspend fun get(): ByBitDTO?
     companion object {
         const val BASE_URL = "https://api.bybit.com"
     }
