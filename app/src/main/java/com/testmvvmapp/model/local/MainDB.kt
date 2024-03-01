d685189a192a955bd5f5df9cc1ba5fa0ce1adc20
FredNekrasov <frednekrasov49@gmail.com>
2024-03-01 18:12:21 +0300

    creating a room database

---
+++ app/src/main/java/com/testmvvmapp/model/local/MainDB.kt
@@ -0,0 +1,18 @@
+package com.testmvvmapp.model.local
+
+import androidx.room.*
+import com.testmvvmapp.model.entities.User
+import com.testmvvmapp.model.local.dao.*
+import com.testmvvmapp.model.type_converters.ResultConverter
+import javax.inject.Singleton
+
+@Singleton
+@Database(entities = [Result::class, User::class], version = 1, exportSchema = false)
+@TypeConverters(ResultConverter::class)
+abstract class MainDB : RoomDatabase() {
+    abstract val resultDao: IResultDao
+    abstract val userDao: IUserDao
+    companion object {
+        const val DB_NAME = "main.db"
+    }
+}
