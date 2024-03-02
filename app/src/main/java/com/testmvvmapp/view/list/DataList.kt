bcf664bcab333c991a10f76bcb0d4887c022d423
FredNekrasov <frednekrasov49@gmail.com>
2024-03-02 14:16:33 +0300

    implementing navigation and adding titles DataList and FavouriteList screens

--- app/src/main/java/com/testmvvmapp/view/list/DataList.kt
+++ app/src/main/java/com/testmvvmapp/view/list/DataList.kt
@@ -8,43 +8,59 @@ import androidx.compose.ui.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.testmvvmapp.R.string
 import com.testmvvmapp.model.service.util.ConnectionStatus
+import com.testmvvmapp.ui.*
 import com.testmvvmapp.view_model.MainViewModel
 
 @Composable
-fun DataList(viewModel: MainViewModel) {
+fun DataList(controller: NavHostController,viewModel: MainViewModel) {
     val state = viewModel.resultSF.collectAsState().value
-    val tryAgain = stringResource(string.tryAgain)
     Box(Modifier.fillMaxSize()) {
         if (state.first == ConnectionStatus.LOADING) CircularProgressIndicator(Modifier.align(Alignment.Center))
         LazyColumn(Modifier.fillMaxSize()) {
+            item {
+                Row(Modifier.fillMaxWidth(),Arrangement.Center,Alignment.CenterVertically) {
+                    FredTextHeader(stringResource(string.shop))
+                }
+                Spacer(Modifier.height(32.dp))
+            }
             items(state.second){ mainInfo ->
                 Item(mainInfo,Modifier.fillMaxWidth())
                 Spacer(Modifier.height(16.dp))
             }
             item {
-                Spacer(Modifier.height(16.dp))
-                TextButton(
-                    { viewModel.getData() },
-                    Modifier.align(Alignment.BottomCenter),
-                    enabled = when(state.first) {
-                        ConnectionStatus.LOADING, ConnectionStatus.SUCCESS -> false
-                        else -> true
-                    }
-                ) {
-                    Text(
-                        when(state.first) {
-                            ConnectionStatus.LOADING -> stringResource(string.LOADING)
-                            ConnectionStatus.SUCCESS -> stringResource(string.SUCCESS)
-                            ConnectionStatus.NO_DATA -> "${stringResource(string.NO_DATA)}\n${tryAgain}"
-                            ConnectionStatus.CONNECTION_ERROR -> "${stringResource(string.CONNECTION_ERROR)}\n${tryAgain}"
-                            ConnectionStatus.NO_INTERNET -> "${stringResource(string.NO_INTERNET)}\n${tryAgain}"
-                            ConnectionStatus.UNKNOWN -> "${stringResource(string.UNKNOWN)}\n${tryAgain}"
-                        }, textAlign = TextAlign.Center
-                    )
+                Row(Modifier.fillMaxWidth(),Arrangement.SpaceAround,Alignment.CenterVertically) {
+                    FredButton({ controller.navigateUp() },stringResource(string.goBack))
+                    this@Box.ShowInternetInfo ({ viewModel.getData() }, state.first)
+                    FredButton({ controller.navigate(ScreenRoutes.Favorites.route) },stringResource(string.favourites))
                 }
+                Spacer(Modifier.height(16.dp))
             }
         }
+    }
+}
+@Composable
+fun BoxScope.ShowInternetInfo(action: () -> Unit,state: ConnectionStatus) {
+    val tryAgain = stringResource(string.tryAgain)
+    TextButton(
+        action,
+        Modifier.align(Alignment.BottomCenter),
+        enabled = when(state) {
+            ConnectionStatus.LOADING, ConnectionStatus.SUCCESS -> false
+            else -> true
+        }
+    ) {
+        Text(
+            when(state) {
+                ConnectionStatus.LOADING -> stringResource(string.LOADING)
+                ConnectionStatus.SUCCESS -> stringResource(string.SUCCESS)
+                ConnectionStatus.NO_DATA -> "${stringResource(string.NO_DATA)}\n${tryAgain}"
+                ConnectionStatus.CONNECTION_ERROR -> "${stringResource(string.CONNECTION_ERROR)}\n${tryAgain}"
+                ConnectionStatus.NO_INTERNET -> "${stringResource(string.NO_INTERNET)}\n${tryAgain}"
+                ConnectionStatus.UNKNOWN -> "${stringResource(string.UNKNOWN)}\n${tryAgain}"
+            }, textAlign = TextAlign.Center
+        )
     }
 }
--- app/src/main/java/com/testmvvmapp/view/list/FavouriteList.kt
+++ app/src/main/java/com/testmvvmapp/view/list/FavouriteList.kt
@@ -3,20 +3,36 @@ package com.testmvvmapp.view.list
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.testmvvmapp.R
+import com.testmvvmapp.ui.*
 import com.testmvvmapp.view_model.MainViewModel
 
 @Composable
-fun FavouriteList(viewModel: MainViewModel) {
+fun FavouriteList(controller: NavHostController,viewModel: MainViewModel) {
     val state = viewModel.resultSF.collectAsState().value
     Box(Modifier.fillMaxSize()) {
         LazyColumn(Modifier.fillMaxSize()) {
+            item {
+                Row(Modifier.fillMaxWidth(),Arrangement.Center,Alignment.CenterVertically) {
+                    FredTextHeader(stringResource(R.string.favourites))
+                }
+                Spacer(Modifier.height(32.dp))
+            }
             items(state.second) { mainInfo ->
                 if(mainInfo.favorite) {
                     Item(mainInfo,Modifier.fillMaxWidth())
                     Spacer(Modifier.height(16.dp))
                 }
+            }
+            item {
+                Row(Modifier.fillMaxWidth(),Arrangement.Center,Alignment.CenterVertically) {
+                    FredButton({ controller.navigateUp() },stringResource(R.string.goBack))
+                }
+                Spacer(Modifier.height(16.dp))
             }
         }
     }
