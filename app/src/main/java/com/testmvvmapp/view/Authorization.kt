86c1620d93a198b650faf02b6c06b55c99c53de1
FredNekrasov <frednekrasov49@gmail.com>
2024-03-02 13:39:47 +0300

    adding a validation and navigation to the Authorization and Registration screens. the work with these screens is completed

--- app/src/main/java/com/testmvvmapp/view/Authorization.kt
+++ app/src/main/java/com/testmvvmapp/view/Authorization.kt
@@ -1,26 +1,58 @@
 package com.testmvvmapp.view
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.*
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.testmvvmapp.R
 import com.testmvvmapp.ui.*
+import com.testmvvmapp.view_model.UserVM
 
 @Composable
-fun Authorization() {
+fun Authorization(controller: NavController, userVM: UserVM) {
+    val userData = userVM.resultSF.collectAsState().value
     var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var isUserNameCorrect by rememberSaveable { mutableStateOf(false) }
+    var isPasswordCorrect by rememberSaveable { mutableStateOf(false) }
     Column(Modifier.fillMaxSize(),Arrangement.Center,Alignment.CenterHorizontally) {
-        FredText(stringResource(R.string.authorization))
+        FredTextHeader(stringResource(R.string.authorization))
         Spacer(Modifier.height(64.dp))
-        FredTextField(userName, { userName = it }, stringResource(R.string.enterUN))
+        TextField(
+            userName,
+            { userName = it },
+            label = { Text(stringResource(R.string.enterUN), fontFamily = FontFamily.Serif) },
+            supportingText = { if(isUserNameCorrect)Text(stringResource(R.string.incorrectUserName), color = MaterialTheme.colorScheme.error, fontFamily = FontFamily.Serif) },
+            isError = isUserNameCorrect,
+            shape = MaterialTheme.shapes.medium,
+            colors = OutlinedTextFieldDefaults.colors()
+        )
         Spacer(Modifier.height(8.dp))
-        FredOTF(password, { password = it }, stringResource(R.string.enterPassword), KeyboardType.NumberPassword)
+        OutlinedTextField(
+            password,
+            { password = it },
+            label = { Text(stringResource(R.string.enterPassword), fontFamily = FontFamily.Serif) },
+            supportingText = { if(isPasswordCorrect) Text(stringResource(R.string.incorrectPassword), color = MaterialTheme.colorScheme.error, fontFamily = FontFamily.Serif) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+            isError = isPasswordCorrect,
+            shape = MaterialTheme.shapes.medium,
+            colors = OutlinedTextFieldDefaults.colors()
+        )
         Spacer(Modifier.height(32.dp))
-        FredButton({  },stringResource(R.string.logIn))
+        FredButton({
+            isUserNameCorrect = userName.isBlank()
+            isPasswordCorrect = password.isBlank() || password.length < 8
+            if(!isUserNameCorrect && !isPasswordCorrect) userVM.authorization(userName, password)
+        },stringResource(R.string.logIn))
+        Spacer(Modifier.height(8.dp))
+        FredButton({ controller.navigate(ScreenRoutes.Registration.route) }, stringResource(R.string.registration))
+        if(userData.first) controller.navigate(ScreenRoutes.Profile.route)
     }
 }
--- app/src/main/java/com/testmvvmapp/view/Registration.kt
+++ app/src/main/java/com/testmvvmapp/view/Registration.kt
@@ -1,32 +1,80 @@
 package com.testmvvmapp.view
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.*
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.testmvvmapp.R.string
+import com.testmvvmapp.model.entities.User
 import com.testmvvmapp.ui.*
+import com.testmvvmapp.view_model.UserVM
 
 @Composable
-fun Registration() {
+fun Registration(controller: NavHostController,userVM: UserVM) {
+    val userData = userVM.resultSF.collectAsState().value
     var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("") }
+    var surname by rememberSaveable { mutableStateOf("") }
+
+    var isUserNameCorrect by rememberSaveable { mutableStateOf(false) }
+    var isPasswordCorrect by rememberSaveable { mutableStateOf(false) }
+    var isEmailCorrect by rememberSaveable { mutableStateOf(false) }
     Column(Modifier.fillMaxSize(),Arrangement.Center,Alignment.CenterHorizontally) {
-        FredText(stringResource(string.registration))
+        FredTextHeader(stringResource(string.registration))
         Spacer(Modifier.height(64.dp))
-        FredTextField(userName, { userName = it }, stringResource(string.enterUN))
+        TextField(
+            userName,
+            { userName = it },
+            label = { Text(stringResource(string.enterUN),fontFamily = FontFamily.Serif) },
+            supportingText = { if(isUserNameCorrect)Text(stringResource(string.incorrectUserName),color = MaterialTheme.colorScheme.error,fontFamily = FontFamily.Serif) },
+            isError = isUserNameCorrect,
+            shape = MaterialTheme.shapes.medium,
+            colors = OutlinedTextFieldDefaults.colors()
+        )
         Spacer(Modifier.height(8.dp))
-        FredOTF(password,{ password = it },stringResource(string.enterPassword),KeyboardType.NumberPassword)
+        OutlinedTextField(
+            password,
+            { password = it },
+            label = { Text(stringResource(string.enterPassword),fontFamily = FontFamily.Serif) },
+            supportingText = { if(isPasswordCorrect) Text(stringResource(string.incorrectPassword),color = MaterialTheme.colorScheme.error,fontFamily = FontFamily.Serif) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+            isError = isPasswordCorrect,
+            shape = MaterialTheme.shapes.medium,
+            colors = OutlinedTextFieldDefaults.colors()
+        )
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(
+            email,
+            { email = it },
+            label = { Text(stringResource(string.enterEmail),fontFamily = FontFamily.Serif) },
+            supportingText = { if(isEmailCorrect) Text(stringResource(string.incorrectEmail),color = MaterialTheme.colorScheme.error,fontFamily = FontFamily.Serif) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            isError = isEmailCorrect,
+            shape = MaterialTheme.shapes.medium,
+            colors = OutlinedTextFieldDefaults.colors()
+        )
+        Spacer(Modifier.height(8.dp))
+        FredTextField(name,{ name = it },stringResource(string.enterName))
+        Spacer(Modifier.height(8.dp))
+        FredTextField(surname,{ surname = it },id = stringResource(string.enterSurname))
         Spacer(Modifier.height(32.dp))
-        FredOTF(password,{ password = it },stringResource(string.enterEmail),KeyboardType.Email)
-        Spacer(Modifier.height(32.dp))
-        FredTextField(password,{ password = it },stringResource(string.enterName))
-        Spacer(Modifier.height(32.dp))
-        FredTextField(password,{ password = it },stringResource(string.enterSurname))
-        Spacer(Modifier.height(32.dp))
-        FredButton({  },stringResource(string.signUp))
+        FredButton({
+            isUserNameCorrect = userName.isBlank()
+            isPasswordCorrect = password.isBlank() || password.length < 8
+            isEmailCorrect = email.isBlank() || !email.contains("@")
+            if(!isEmailCorrect && !isUserNameCorrect && !isPasswordCorrect) userVM.registration(User(userName, password, email, name, surname))
+        },stringResource(string.signUp))
+        Spacer(Modifier.height(8.dp))
+        FredButton({ controller.navigate(ScreenRoutes.Authorization.route) }, stringResource(string.goBack))
+        if(userData.first) controller.navigate(ScreenRoutes.Profile.route)
     }
 }
