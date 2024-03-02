2d3cddf117a2ed50d180a509b90632ad0aff655b
FredNekrasov <frednekrasov49@gmail.com>
2024-03-02 12:09:27 +0300

    creating views to display user data

---
+++ app/src/main/java/com/testmvvmapp/view/profile/Profile.kt
@@ -0,0 +1,27 @@
+package com.testmvvmapp.view.profile
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.testmvvmapp.R.string
+import com.testmvvmapp.ui.*
+import com.testmvvmapp.view_model.UserVM
+
+@Composable
+fun Profile(controller: NavHostController,userVM: UserVM) {
+    val user = userVM.resultSF.collectAsState().value.second
+    Column(Modifier.fillMaxSize(),Arrangement.Top,Alignment.CenterHorizontally) {
+        Text(stringResource(string.profile),style = MaterialTheme.typography.titleLarge,color = MaterialTheme.colorScheme.tertiaryContainer)
+        Spacer(Modifier.height(16.dp))
+        UserInfo(user, Modifier.fillMaxWidth())
+        Spacer(Modifier.height(16.dp))
+        Row(Modifier.fillMaxWidth(), Arrangement.SpaceAround, Alignment.CenterVertically) {
+            FredButton({ controller.navigate(ScreenRoutes.DataList.route) }, stringResource(string.shop))
+            FredButton({ controller.navigate(ScreenRoutes.Favorites.route) }, stringResource(string.favourites))
+        }
+    }
+}
---
+++ app/src/main/java/com/testmvvmapp/view/profile/UserInfo.kt
@@ -0,0 +1,31 @@
+package com.testmvvmapp.view.profile
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.testmvvmapp.R.string
+import com.testmvvmapp.model.entities.User
+import com.testmvvmapp.ui.FredCardView
+
+@Composable
+fun UserInfo(user: User?,modifier: Modifier) {
+    if (user != null) {
+        Box(modifier) {
+            FredCardView(Modifier.matchParentSize(),MaterialTheme.colorScheme.tertiaryContainer,MaterialTheme.colorScheme.onTertiaryContainer)
+            Column(Modifier.fillMaxSize().padding(16.dp).padding(end = 32.dp)) {
+                Text("${stringResource(string.userName)}: ${user.userName}", style = MaterialTheme.typography.titleLarge, color = MaterialTheme.colorScheme.onTertiaryContainer)
+                Spacer(Modifier.height(8.dp))
+                Text("${stringResource(string.password)}: ${user.password}", style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.onTertiaryContainer)
+                Spacer(Modifier.height(8.dp))
+                Text("${stringResource(string.email)}: ${user.email}", style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.onTertiaryContainer)
+                Spacer(Modifier.height(8.dp))
+                Text("${stringResource(string.name)}: ${user.firstName}", style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.onTertiaryContainer)
+                Spacer(Modifier.height(8.dp))
+                Text("${stringResource(string.surname)}: ${user.lastName}", style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.onTertiaryContainer)
+            }
+        }
+    }
+}
