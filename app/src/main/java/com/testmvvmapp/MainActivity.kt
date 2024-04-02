bf9abc987472f61669ce2f258a5297bcc675b832
FredNekrasov <frednekrasov49@gmail.com>
2024-04-02 19:14:26 +0300

    refactoring the MainActivity

--- app/src/main/java/com/testmvvmapp/MainActivity.kt
+++ app/src/main/java/com/testmvvmapp/MainActivity.kt
@@ -3,26 +3,26 @@ package com.testmvvmapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.*
+import com.testmvvmapp.bybit.presentation.DataList
+import com.testmvvmapp.bybit.presentation.FavouriteList
+import com.testmvvmapp.bybit.presentation.viewModel.BybitVM
 import com.testmvvmapp.ui.ScreenRoutes
 import com.testmvvmapp.ui.theme.TestMVVMAppTheme
 import com.testmvvmapp.user.presentation.Authorization
 import com.testmvvmapp.user.presentation.Registration
-import com.testmvvmapp.bybit.presentation.DataList
-import com.testmvvmapp.bybit.presentation.FavouriteList
 import com.testmvvmapp.user.presentation.profile.Profile
-import com.testmvvmapp.bybit.presentation.viewModel.BybitVM
 import com.testmvvmapp.user.presentation.viewModel.UserVM
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class MainActivity : ComponentActivity() {
-    private val bybitVM: BybitVM by viewModels()
-    private val userVM: UserVM by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bybitVM: BybitVM = getViewModel()
+        val userVM: UserVM = getViewModel()
         setContent {
             TestMVVMAppTheme {
                 Surface(Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
